c917629c55f0baff295391e53604603a821df78d
Valerii Strilets <valerii.strilets@gmail.com>
2023-11-27 23:27:16 +0200

    feat: add fastify provider

--- packages/fastify/vite.config.ts
+++ packages/fastify/vite.config.ts
@@ -1,7 +1,7 @@
 import { viteConfig } from '../../configs/vite.config.base';
 
 export default viteConfig({
-  name: 'express',
+  name: 'fastify',
   entry: 'src/index.ts',
   package: await import('./package.json'),
 });
