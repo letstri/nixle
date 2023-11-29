5c4672ff1eba5bfdb8c502e1a1a0bc12af01f5fa
Valerii Strilets <valerii.strilets@gmail.com>
2023-11-29 22:45:24 +0200

    feat: add comments

--- packages/nixle/dist/createProvider.d.ts
+++ packages/nixle/dist/createProvider.d.ts
@@ -1,15 +1,78 @@
 import type { CookieSerializeOptions } from 'cookie';
 import type { HTTPMethod } from '.';
 export type Handler = (params: {
+    /**
+     * Request
+     */
     request: any;
+    /**
+     * Response
+     */
     response: any;
+    /**
+     * Path parameters
+     *
+     * @example
+     * '/users/:id'
+     *
+     * @returns
+     * { id: '1' }
+     */
     params: Record<string, string>;
+    /**
+     * Query parameters
+     *
+     * @returns
+     * { name: 'John' }
+     *
+     * @example
+     * '/users?name=John'
+     */
     query: Record<string, string | string[]>;
+    /**
+     * Set status code
+     *
+     * @param code
+     * @default 200
+     *
+     * @example
+     * setStatusCode(404);
+     */
     setStatusCode: (code: number) => void;
+    /**
+     * Set header
+     *
+     * @param key
+     * @param value
+     *
+     * @example
+     * setHeader('Content-Type', 'application/json');
+     */
     setHeader: (key: string, value: string) => void;
+    /**
+     * Set cookie
+     *
+     * @param key
+     * @param value
+     * @param options
+     *
+     * @example
+     * setCookie('token', '123');
+     * setCookie('token', '123', { httpOnly: true });
+     */
     setCookie: (key: string, value: string, options?: CookieSerializeOptions) => void;
 }) => Promise<any> | any;
 export interface Provider<Server> {
+    /**
+     * Register a route
+     *
+     * @param method
+     * @param path
+     * @param handler
+     *
+     * @example
+     * request('get', '/users', () => ({ message: 'Hello world!' }));
+     */
     request: (method: Lowercase<HTTPMethod>, path: string, handler: Handler) => void;
     server: Server;
 }
